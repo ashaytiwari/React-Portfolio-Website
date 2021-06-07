cbf77ef78058ef95fcbf7711a11ab92173af6204
ashaytiwari <ashaytiwari2001@gmail.com>
2021-06-07 10:57:36 +0530

    feat: Profile Img and Intro text set for about section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import "./App.css";
-import HomePage from "./container/HomePage/HomePage";
+import './App.css'
+import HomePage from './container/HomePage/HomePage'
 
 function App() {
   return (
-    <div className={"App"}>
+    <div className={'App'}>
       <HomePage />
-      {/* <p className={"ubuntuBold"}>Portfolio Website</p>
-      <p>Jai Srila Prabhupad</p>
-      <p className={"ubuntuLight"}>Hare Krishna</p>
-      <p className={"ubuntuMedium"}>Hare Krishna</p>
-      <p className={"ubuntuRegular"}>Hare Krishna</p>
-      <p className={"yellowtail"}>Hare Krishna</p> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
