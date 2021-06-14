d9f3a3153d385f0d827761d9f2bdbc310c23997b
ashaytiwari <ashaytiwari2001@gmail.com>
2021-06-14 13:26:35 +0530

    Revert "feat: Email Js"
    This reverts commit 25644142c27958c929ca922c54b5c3864fd5e255.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,11 @@
 import "./App.css";
-import React from "react";
 import HomePage from "./container/HomePage/HomePage";
-import { SnackbarProvider } from "notistack";
-import { Slide, IconButton, makeStyles } from "@material-ui/core";
-import {
-  SnackbarAutohideDuration,
-  SnackbarDuplicate,
-  SnackbarHorizontalPosition,
-  SnackbarVerticalPosition
-} from "./GlobalUtils/configFileAccessor";
-import { CloseOutlined } from "@material-ui/icons";
-
-const useStyles = makeStyles({
-  error: { backgroundColor: "#ff726f" }
-});
 
 function App() {
-  const classes = useStyles();
-  const notistackRef = React.createRef();
-  const onClickDismiss = (key) => () => {
-    notistackRef.current.closeSnackbar(key);
-  };
-
   return (
-    <SnackbarProvider
-      anchorOrigin={{
-        vertical: SnackbarVerticalPosition,
-        horizontal: SnackbarHorizontalPosition
-      }}
-      preventDuplicate={SnackbarDuplicate}
-      autoHideDuration={SnackbarAutohideDuration}
-      TransitionComponent={Slide}
-      ref={notistackRef}
-      classes={{
-        variantError: classes.error
-      }}
-      action={(key) => (
-        <IconButton onClick={onClickDismiss(key)} className={"iconButton"}>
-          <CloseOutlined className={"close"} />{" "}
-        </IconButton>
-      )}
-    >
-      <div className={"App"}>
-        <HomePage />
-      </div>
-    </SnackbarProvider>
+    <div className={"App"}>
+      <HomePage />
+    </div>
   );
 }
 
