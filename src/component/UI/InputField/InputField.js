25644142c27958c929ca922c54b5c3864fd5e255
ashaytiwari <ashaytiwari2001@gmail.com>
2021-06-14 13:25:33 +0530

    feat: Email Js

--- src/component/UI/InputField/InputField.js
+++ src/component/UI/InputField/InputField.js
@@ -1,6 +1,6 @@
 import React from "react";
-import TelegramIcon from "@material-ui/icons/Telegram";
 import { TextField } from "@material-ui/core";
+import styles from "./InputField.module.css";
 
 function InputField(props) {
   return (
@@ -9,6 +9,11 @@ function InputField(props) {
       name={props.name}
       label={props.label}
       placeholder={props.placeholder}
+      variant={props.variant}
+      className={styles.input}
+      autoComplete={"off"}
+      value={props.value}
+      onChange={props.onChange}
     />
   );
 }
