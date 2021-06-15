67ed03355f90ae95ade23159a13b578a92e9e60f
ashaytiwari <ashaytiwari2001@gmail.com>
2021-06-15 11:15:12 +0530

    feat: Email Js Integration

--- src/GlobalUtils/globalFunction.js
+++ src/GlobalUtils/globalFunction.js
@@ -1,3 +1,8 @@
+/**
+ * function to find near by multiple of 3
+ * @param {*} number
+ * @returns near by multiple
+ */
 export const findMultipleOf3 = (number) => {
   //   console.log(number);
   if (number % 3 === 0) {
@@ -9,4 +14,29 @@ export const findMultipleOf3 = (number) => {
       }
     }
   }
-};
+}; // end of findMultipleOf3
+
+/**
+ * function to test email validation
+ * @param {*} email
+ * @returns true/false
+ */
+export const emailValidation = (email) => {
+  if (
+    !/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+      email
+    )
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}; // end of emailValidation
+
+/**
+ * Function to reset the upcoming fields
+ * @param  {...any} setState is coming as props
+ */
+export const resetForm = (...props) => {
+  props.forEach((setState) => setState(""));
+}; //end of resetForm
--- src/GlobalUtils/globalconstants.js
+++ src/GlobalUtils/globalconstants.js
@@ -1,2 +1,9 @@
-export const ResumeTooltipMsg = 'Click to download resume'
-export const KritinUrl = 'https://kritin.in'
+export const ResumeTooltipMsg = "Click to download resume";
+export const KritinUrl = "https://kritin.in";
+export const NameIsRequired = "Name is Required";
+export const EmailIsRequired = "Email is Required";
+export const MessageIsRequierd = "Message is Required";
+export const EmailIsNotValid = "Email is not valid";
+export const DataSuccessSubmit = "Data successfully submitted";
+export const DataErrorSubmit = "Error in submitting data";
+export const NetworkError = "Network Error";
